299cfb7041986ae0dceaa849c5db15286dab6c45
jaehyunnam <skawogus924@gmail.com>
2024-08-07 01:02:43 +0900

    Swagger 추가 및 MVC패턴 적용

---
+++ src/main/java/com/deerear/deerear/DeerearApplication.java
@@ -0,0 +1,12 @@
+package com.deerear.deerear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeerearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeerearApplication.class, args);
+	}
+}

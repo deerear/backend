f0bb7a102060f5b7aa1a2f0a3b9c38ceb3c20bd1
jaehyunnam <skawogus924@gmail.com>
2024-08-07 23:20:21 +0900

    Update project structure

--- src/main/java/com/deerear/deerear/config/SwaggerConfig.java
+++ src/main/java/com/deerear/deerear/config/SwaggerConfig.java
@@ -1,22 +1,37 @@
 package com.deerear.deerear.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class SwaggerConfig implements WebMvcConfigurer {
-
+public class SwaggerConfig {
     @Bean
-    public OpenAPI OpenApi() {
-
+    public OpenAPI openAPI() {
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
         return new OpenAPI()
-                .info(new Info().title("Dearear API")
-                        .description("Dearear api 입니다.")
-                        .version("v0.0.1"));
+                .components(new Components())
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+    private Info apiInfo() {
+        return new Info()
+                .title("API Test") // API의 제목
+                .description("Let's practice Swagger UI") // API에 대한 설명
+                .version("1.0.0"); // API의 버전
     }
 }
---
+++ src/main/java/com/deerear/deerear/service/ResponseEstimateService.java
@@ -0,0 +1,11 @@
+package com.deerear.deerear.service;
+
+import com.deerear.deerear.repository.ResponseEstimateRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ResponseEstimateService {
+
+}

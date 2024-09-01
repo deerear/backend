9aca541f971fc22fbec88d93d079ea1d74e02246
jaehyunnam <skawogus924@gmail.com>
2024-09-01 23:25:34 +0900

    feat: Add test package and files

---
+++ src/test/java/com/deerear/app/DeerearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deerear.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeerearApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // 빈 테스트 메서드
+    }
+}

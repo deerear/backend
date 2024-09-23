e43dab3432255502629d286886c78fb9963f2ca4
unknowoon <wgb1223@gmail.com>
2024-09-23 23:01:50 +0900

    fix: test

--- src/test/java/com/deerear/app/DeerearApplicationTests.java
+++ src/test/java/com/deerear/app/DeerearApplicationTests.java
@@ -1,13 +1,8 @@
-//package com.deerear.deerear;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class DeerearApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.deerear.app;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeerearApplicationTests {
+
+}

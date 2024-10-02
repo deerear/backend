3bc873a91fb300c4513039c3ed505bdd81f30473
unknowoon <wgb1223@gmail.com>
2024-10-02 17:22:24 +0900

    fix: 정리

--- src/main/java/com/deerear/app/controller/CommentsController.java
+++ src/main/java/com/deerear/app/controller/CommentsController.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;

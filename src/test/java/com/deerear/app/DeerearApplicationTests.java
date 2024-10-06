1f56fed5771660829191c00cb8a5d1e7377a8590
S <94242504+DongGuk-Seo@users.noreply.github.com>
2024-10-06 17:49:06 +0900

    Revert "fix: test"

--- src/test/java/com/deerear/app/DeerearApplicationTests.java
+++ src/test/java/com/deerear/app/DeerearApplicationTests.java
@@ -1,8 +1,19 @@
 package com.deerear.app;
 
+<<<<<<< HEAD
+=======
+import org.junit.jupiter.api.Test;
+>>>>>>> main
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DeerearApplicationTests {
 
+<<<<<<< HEAD
+=======
+    @Test
+    void contextLoads() {
+        // 빈 테스트 메서드
+    }
+>>>>>>> main
 }

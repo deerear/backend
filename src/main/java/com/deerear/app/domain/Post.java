10b8e894d1114058392893a43bab1b195b7a3434
unknowoon <wgb1223@gmail.com>
2024-10-09 23:17:42 +0900

    fix: CommentCount 증감 추가ㅎ

--- src/main/java/com/deerear/app/domain/Post.java
+++ src/main/java/com/deerear/app/domain/Post.java
@@ -51,4 +51,14 @@ public class Post extends Likeable {
     public TargetType getTargetType() {
         return TargetType.POST;
     }
+
+    public void incrementCommentCount() {
+        this.commentCount++;
+    }
+
+    public void decrementCommentCount() {
+        if (this.commentCount > 0) {
+            this.commentCount--;
+        }
+    }
 }

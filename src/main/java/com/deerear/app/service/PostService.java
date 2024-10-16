68978af355ea1fc253110739f2d717dc9dbbffdb
DongGuk-Seo <failnxid@gmail.com>
2024-10-16 20:55:20 +0900

    fix: import conflict

--- src/main/java/com/deerear/app/service/PostService.java
+++ src/main/java/com/deerear/app/service/PostService.java
@@ -27,6 +27,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static com.deerear.app.util.StaticFiles.saveImage;
 
